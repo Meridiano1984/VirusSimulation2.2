752e4c74d0ae17e028bea4bb248f756e7653a810
Meridiano1984 <259125@student.pwr.edu.pl>
2021-06-10 00:08:57 +0200

    dodanie

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -40,15 +40,18 @@ public class Main {
                 System.out.print("ROZMIAR POLA(KWADRAT, MINIMALNY ROZMIAR TO 2): ");
                 size =scanner.nextInt();
                 if(size<2){throw new InputMismatchException();}
-                System.out.print("LICZBA MEDYKÓW: ");
-                numberOfMedic = scanner.nextInt();
-                if(numberOfMedic<0){throw new InputMismatchException();}
-                System.out.print("LICZBA CHORYCH: ");
-                numberOfSick = scanner.nextInt();
-                if(numberOfSick<0){throw new InputMismatchException();}
                 System.out.print("LICZBA ZDROWYCH: ");
                 numberOfHealthy = scanner.nextInt();
                 if(numberOfHealthy<0){throw new InputMismatchException();}
+                System.out.print("LICZBA CHORYCH: ");
+                numberOfSick = scanner.nextInt();
+                if(numberOfSick<0){throw new InputMismatchException();}
+                System.out.print("LICZBA MEDYKÓW: ");
+                numberOfMedic = scanner.nextInt();
+                if(numberOfMedic<0){throw new InputMismatchException();}
+                System.out.println("LICZBA CHORYCH MEDYKÓW(SUGEROWANA=0): ");
+                numberOfSickMedic = scanner.nextInt();
+                if(numberOfSickMedic<0){throw new InputMismatchException();}
                 System.out.print("LICZBA PRZESZKÓD: ");
                 numberOfObstacle = scanner.nextInt();
                 if(numberOfObstacle<0){throw new InputMismatchException();}
