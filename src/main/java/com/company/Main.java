f7fe9cb9da0d94bc570aaec35d18cf608d9f062c
Meridiano1984 <259125@student.pwr.edu.pl>
2021-06-10 12:02:47 +0200

    dopianie komentarzy do MAIN()

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -36,7 +36,7 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
 
-        /*WYSWIETLENIE MENU I POBRANIE ZMIENNYCH DOTYCZACYCH SYMULACJI OD UZYTKOWNIKA*/
+        /*WYSWIETLENIE MENU I POBRANIE ZMIENNYCH DOTYCZACYCH SYMULACJI OD UZYTKOWNIKA. MENU JEST ZABEZPIECZONE PRZED WPISYWANIEM NIEODPOWIEDNICH WARTOSCI*/
         boolean end=true;
         do {
             try {
@@ -90,37 +90,48 @@ public class Main {
 
 
 
+        //TWORZENIE WIRUSA O ZADANEJ SMIERTELNOSCI
+        Virus virus = new Virus(deathRateSick);
 
-        Virus virus = new Virus(deathRateSick);        //     INICJALIZACJA KLASY WIRUS
-        numberOfObjects =numberOfHealthy+numberOfMedic+numberOfSickMedic+numberOfObstacle+numberOfSick;                                           //     WYBIERAMY LICZBE OBIEKTOW
+        //SUMWOWANIE LICZBY OBIEKTOW WYSTEPUJACYCH W SYMULACJI
+        numberOfObjects =numberOfHealthy+numberOfMedic+numberOfSickMedic+numberOfObstacle+numberOfSick;
+
+        //TWORZENIE LIST ZAWIERJACYCH OBIEKTY WYSTEPUJACE W SYMULACJI
         GameObjectList gameObjectList = new GameObjectList(numberOfObjects, IHealthyCreator.createHealthy(numberOfHealthy), IObstacleCreator.createObstacle(numberOfObstacle), IMedicCreator.createMedic(numberOfMedic), ISickCreator.createSick(numberOfSick, virus), ISickMedicCreator.createSickMedic(numberOfSickMedic, virus), virus);
-//      LinkedList<GameObject> bigList = gameObjectList.bigListCreator(gameObjectList.getHealthyList(), gameObjectList.getMedicList(), gameObjectList.getSickList(), gameObjectList.getSickMedicList());                                                                                                                                                 // utworzeni listy zawierajace wszystkie gameobjecty z wyjatkiem obstacle sluży do przmeiszczania obiektow
-        LinkedList<GameObject> movingList = gameObjectList.movingListCreator(gameObjectList.getHealthyList(), gameObjectList.getMedicList(), gameObjectList.getSickList());
-        Area[][] map = Area.mapGenerator(size, size);                                  //TWORZENIE MAPY O WYMIARACH size NA size ZA POMOCA SPECJALNEJ FUNKCJI
+
+        //TWORZENIE MAPY NA KTOREJ BEDA PORUSZAC SIE OBIEKTY
+        Area[][] map = Area.mapGenerator(size, size);
 
 
 
-        SickHandler sickHandler = new SickHandler(map, null, size);                  //TWORZENIE OBIEKTOW HENDLEROW NA KTORYCH RZECZ BEDA WYWOLYWANE FUNKCJE
+
+        //TWORZENIE HANDLEROW ODPOWIADJACYCH ZA AKCJE DANYCH OBIEKTOW
+        SickHandler sickHandler = new SickHandler(map, null, size);
         HealthyHandler healthyHandler = new HealthyHandler(map, null, size);
         SickMedicHandler sickMedicHandler = new SickMedicHandler(map, null, size);
         MedicHandler medicHandler = new MedicHandler(map, null, size);
         MoveHandler moveHandler = new MoveHandler(map, null);
 
-
+        //ROZMIESZCZENIE OBIEKTOW Z LIST NA TABLICE
         map = map[0][0].mapGameObjectInitialization(map, gameObjectList.getObstacleList(), gameObjectList.getHealthyList(), gameObjectList.getSickList(), gameObjectList.getMedicList(), gameObjectList.getSickMedicList(), size);       //INICJALIZOWANIE MAPY OBIEKATMI Z LIST
 
-
+        //DEKALRACJA I INCJALIZACJIA OKNA GRAFICZNEGO WYSWIETLAJACEGO SYMULACJE
         Window window = new Window(map,numberOfObjects,numberOfHealthy,numberOfSick,numberOfMedic,numberOfSickMedic,numberOfObstacle,gameObjectList, virus);
         window.repaint();
         Thread.sleep(2000);
 
+        //ZAPIS DANYCH DO PLIKU .TXT
         sentence = ("Tura nr " + (0) + "\nIlość obiektów: " + numberOfObjects +"\nIlość chorych: " + numberOfSick + "\nIlość zdrowych: " + numberOfHealthy + "\nIlość Medyków: " + numberOfMedic + "\nIlość Chorych Medyków: " + numberOfSickMedic + "\nIlość przeszkód: " + numberOfObstacle +"\n\n");
         fileHandler.programDataFileWriter(sentence,fileInformations);
 
+        //WYSWITLENIE MAPY W KONSOLI PO INICJALIZACJI OBIEKTAMI
         Area.MapDisplay(map, size);
+
+
+        //WLASCIWE ROZPOCZECIE DZIALANIA SYMULACJI
         for (int i = 0; i <iteration || Area.isEnd(gameObjectList); i++) {
 
-
+            //ZEROWANIE LICZNIKOW ZAPISUJACYCH DANE
             Medic.medicToFile = 0;
             SickMedic.sickMedicToFile = 0;
             Healthy.healthyToFile = 0;
@@ -128,68 +139,63 @@ public class Main {
             SickMedic.deathSickMedicToFile = 0;
             Sick.deathSickToFile = 0;
 
+            //RUCH HEALTHY ZASIEG 2 POLA
             map = moveHandler.twoFieldHealthyMoveHandler(map, size);
+
+            //RUCH MEDIC ZASIEG 2 POLA
             map = moveHandler.twoFieldMedicMoveHandler(map, size);
+
+            //RUCH SICK ZASIEG 1 POLE
             map = moveHandler.oneFieldSickMoveHandler(map, size);
 
-//            System.out.println(" ");
-//            System.out.println(" ");
-//            System.out.println("Ruch                              TURA NR:"+(i+1)+   " TYLE SICK: " +gameObjectList.getSickList().size()+ "    TYLE HEALTHY:"  + gameObjectList.getHealthyList().size()  );
-//            Area.MapDisplay(map, size);
-//
+            //ZARAZANIE. ZAMIANA ZDROWYCH W SICK JESLI W ZASIEGU JEDNEGO POLA ZNADUJE SIE CHORY
             healthyHandler.transformationToSick(map,virus,gameObjectList.getHealthyList(),gameObjectList.getSickList(),gameObjectList);
-////
-//            System.out.println(" ");
-//            System.out.println(" ");
-//            System.out.println("Zarazenie Zdrowego                 TURA NR:"+(i+1)+"   TYLE SICK: " +gameObjectList.getSickList().size()+ "    TYLE HEALTHY: " + gameObjectList.getHealthyList().size()  );
-//            Area.MapDisplay(map, size);
-//
+
+            //ZARAZANIE MEDIC. ZAMIANA W SICKMEDIC JESLI W ZASIEGU JEDNEGO POLA ZNAJDUJA SIE CO NAJMNIEJ 3 SICK
             medicHandler.transformationToSickMedic(map, virus, gameObjectList.getMedicList(), gameObjectList.getSickMedicList(),gameObjectList);
 
-//            System.out.println(" ");
-//            System.out.println(" ");
-//            System.out.println("Zarazenie Medyka                    TURA NR:"+(i+1)+"   TYLE SICKMEDIC: " +gameObjectList.getSickMedicList().size()+ "    TYLE MEDIC: " + gameObjectList.getMedicList().size());
-//            Area.MapDisplay(map, size);
-//
-
+            //ZWIEKSZENIE WARTOSCI LICZNIKA CZASU ZARAZENIA SICKMEDIC O JEDEN (MAX 3)
             sickMedicHandler.checkingNumberOfIteration(gameObjectList.getSickMedicList());
 
+            //SMIERC LUB OZDROWIENIE. SPRAWDZENIE CZY PO 3 TURACH OD ZARAZENIA SICKMEDIC W JEGO OTOCZENIU ZNAJDUJE SIE 3 LUB WIECEJ SICK JESLI TAK OBIEKT GINIE W PRZECIWNYM RAZIE ZAMIENIA SIE W MEDIC
             sickMedicHandler.transformationToMedicOrDying(map,gameObjectList.getMedicList(),gameObjectList.getSickMedicList(),medicHandler,size,gameObjectList);
-//
-//            System.out.println(" ");
-//            System.out.println(" ");
-//            System.out.println("Leczenie lub usuwanie chorego medyka       TURA NR:" +(i+1) + "    TYLE CHPRYCH MEDYKOW: " + gameObjectList.getSickMedicList().size() + "    TYLE MEDYKOW:" + gameObjectList.getMedicList().size());
-//            Area.MapDisplay(map, size);
-//
-            sickHandler.transformationToHealthy(map, gameObjectList.getHealthyList(), gameObjectList.getSickList(),gameObjectList);
-//
-//            System.out.println(" ");
-//            System.out.println(" ");
-//            System.out.println("Leczenie                             TURA NR:"+(i+1)+"   TYLE SICK: " +gameObjectList.getSickList().size()+ "    TYLE HEALTHY: " + gameObjectList.getHealthyList().size());
 
+            //ULECZENIE SICK. JESLI W ODLEGLOSCI 2 POL OD SICK ZNAJDUJE SIE MEDICK TO SICK ZOSTAJE ZASTAPIONY HEALTHY
+            sickHandler.transformationToHealthy(map, gameObjectList.getHealthyList(), gameObjectList.getSickList(),gameObjectList);
+
+            //SMIERC SICK. TURA ZABIJAJACA CZESC SCIK
             sickHandler.virusKillSick(map,gameObjectList.getSickList(),gameObjectList);
 
-
+            //WYSWIETLENIE MAPY W KONSOLI WRAZ Z KROTKIM OPISEM
             System.out.println("");
             System.out.println("KONIEC TURY:"+(i+1)+" SICK: "+ gameObjectList.getSickList().size()+" HEALTHY:"+gameObjectList.getHealthyList().size()+" MEDIC:"+gameObjectList.getMedicList().size()+" SICKMEDIC: "+ gameObjectList.getSickMedicList().size());
             Area.MapDisplay(map, size);
 
+            //ZAPISANIE DANYCH Z DANEJ TURY DO PLIKU
             sentence = ("Tura nr " + (i+1) + "\nWyzdrowiało tyle chorych: " + Healthy.healthyToFile + "\nZachorowało tyle zdrowych: " + Sick.sickToFile + "\nWyleczyło się tyle Lekarzy z choroby: " + Medic.medicToFile + "\nTyle Medyków zachorowało: " + SickMedic.sickMedicToFile + "\nUmarło tyle chorych: " + Sick.deathSickToFile + "\nUmarło tyle zarażonych lekarzy: " + SickMedic.deathSickMedicToFile + "\nIlość chorych: " + gameObjectList.getSickList().size() + "\nIlość zdrowych: " + gameObjectList.getHealthyList().size() + "\nIlość Medyków: " + gameObjectList.getMedicList().size() + "\nIlość Chorych Medyków: " + gameObjectList.getSickMedicList().size() +"\n\n");
             fileHandler.programDataFileWriter(sentence,fileInformations);
 
+            //REGENERACJA OBIEKTOW PO WYKONANIU RUCHU
             gameObjectList.regeneration(gameObjectList);
             gameObjectList.virusSpreadingRegeneration(gameObjectList);
 
-
+            //SPRAWDZNEIE WARUNKU KONCOWEGO
             if(Area.isEnd(gameObjectList)){
                 break;
             }
 
+            //ZWIEKSZENIE LICZNIKA ZAPISUJACEGO LICZBE TUR DO PLIKU
             Area.numberOfIteration++;
+
+            //ZAKTUALIZOWANIE SYTUACJI W OKNIE GRAFICZNYM
             window.repaint();
+
+            //ZATRZYMANIE CZASU NA 2 SEKUNDY ABY UZYTKOWNIK MIAL CZAS ZOBACZYC ZMIANY
             Thread.sleep(2000);
 
         }
+
+        //WYSWIETLENIE OSTATECZNEJ SYTUACJI W OKNIE GRAFICZNYM
         window.repaint();
 
     }
